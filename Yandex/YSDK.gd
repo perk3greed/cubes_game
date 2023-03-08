c9331d859799ed00c4a597b93d1513abd2393ccb
DISN-kolo <arrtemsky@gmail.com>
2023-03-08 17:07:35 +0100

    rewrite of js integration

--- Yandex/YSDK.gd
+++ Yandex/YSDK.gd
@@ -1,8 +1,8 @@
 extends Node
 
 var window = JavaScript.get_interface("window")
-var returned_data = {}
-
+var returned_data
+var local_returned_data
 
 var init_SDK_callback = JavaScript.create_callback(self, "init_SDK")
 var get_player_callback = JavaScript.create_callback(self, "get_Player")
@@ -12,38 +12,26 @@ var get_data_callback = JavaScript.create_callback(self, "get_Data")
 var local_set_data_callback = JavaScript.create_callback(self, "local_set_Data")
 var local_get_data_callback = JavaScript.create_callback(self, "local_get_Data")
 
-
 var ready_to_get_data = false
 var first_get_callback = true
 var starting_player_logged = false
 var yandex_is_connected = false
-#var initError
 
 func _ready() -> void:
-#	window = JavaScript.get_interface("window")
 	print("Initializing SDK...")
 	if window.SDKError:
-		ready_to_get_data = true
+		print("api couldn't connect. init isn't possible")
 		return
 	window.initSDK(init_SDK_callback)
 	
 func init_SDK(args):
-#	window = JavaScript.get_interface("window")
 	print("init args from callback")
 	print(args)
 	if args[0] == true:
-#		var ready_to_get_player = 1
 		window.getPlayer(get_player_callback)
 		yandex_is_connected = true
-#	else:
-##		yandex_is_connected = false
-#		ready_to_get_data = true
-#	if args == null:
-#		ready_to_get_data = true
-#	ready_to_get_data = true
 
 func get_Player(args):
-#	window = JavaScript.get_interface("window")
 	print("player args from callback")
 	print(args)
 	print(args[0])
@@ -57,7 +45,6 @@ func get_Mode(args):
 		starting_player_logged = true
 
 func set_Data_js(dataToWrite):
-#	window = JavaScript.get_interface("window")
 	print("beginning data set from godot")
 	window.setData(set_data_callback, dataToWrite)
 
@@ -66,38 +53,35 @@ func set_Data(args):
 	print(args)
 
 func get_Data_js():
-#	window = JavaScript.get_interface("window")
 	print("beginning data get from godot")
 	return window.getData(get_data_callback)
 
 func get_Data(args):
 	print("get data args from callback")
 	print(args)
-	# possibly make here an if to return null if null
 	returned_data = JSON.parse(args[0])
 	if first_get_callback:
 		first_get_callback = false
 		return
 	ready_to_get_data = true
 
-func local_set_Data(args):
-	print("local set data callback")
-	print(args)
-
 func local_set_Data_js(dataToWrite):
 	print("beginning local data set from godot")
 	window.localSetData(local_set_data_callback, dataToWrite)
 
-func local_get_Data(args):
-	print("local get data callback")
+func local_set_Data(args):
+	print("local set data callback")
 	print(args)
-	returned_data = JSON.parse(args[0])
-	ready_to_get_data = true
 
 func local_get_Data_js():
 	print("beginning local data get from godot")
 	return window.localGetData(local_get_data_callback)
 
+func local_get_Data(args):
+	print("local get data callback")
+	print(args)
+	local_returned_data = JSON.parse(args[0])
+	ready_to_get_data = true
 
 func generate_ready_dict_in_string(td, cl):
 	print('{"tutorial_done": ' + str(td).to_lower() + ', "current_level": ' + str(cl) + '}')
--- menu/TheGAME.gd
+++ menu/TheGAME.gd
@@ -3,11 +3,6 @@ extends Node2D
 var current_level
 var tutorial_done
 var rng = RandomNumberGenerator.new()
-var data_missing = true
-var our_data
-var going_local = false
-#var first_launch = false
-
 
 signal start_level_data(level_data)
 signal play_tutorial
@@ -16,14 +11,10 @@ signal unmute_sounds
 
 var master_sound = AudioServer.get_bus_index("Master")
 
-
-
 onready var levels_listed = "res://Bricks/desired_items.gd"
 var scene = preload("res://levels/lvl1.tscn")
 var menu_scene = preload("res://Bricks/animated textures/start_menu.tscn")
 
-
-#levels range from 0 to 5  
 var lvls = {
 	1 : [2, 1], 
 	2 : [0, 5, 0, 0, 5, 0],
@@ -59,8 +50,6 @@ var lvls = {
 	
 }
 
-
-
 func _notification(what):
 	match what:
 		MainLoop.NOTIFICATION_WM_FOCUS_OUT:
@@ -68,124 +57,11 @@ func _notification(what):
 		MainLoop.NOTIFICATION_WM_FOCUS_IN:
 			pass
 
-
-
-
-
-
-
-
-
-
 func _ready():
 	Events.connect("sound_control_pressed", self, "sound_change")
 	Events.connect("start_button_pressed", self, "change_children")
 	Events.connect("next_button_pressed", self, "go_back_to_menu")
 	JavaScript.eval("console.log('_ready() moment')")
-#	var contador = 0
-#	while Ysdk.starting_player_logged != 1:
-#		print("starting player not logged in")
-#		contador += 1
-#		if contador >= 10000:
-#			print("quitting _ready")
-#			break
-#		print(Ysdk.get_Data_js())
-#	print("STARTING PLAYER IS LOGGED IN")
-#	print(Ysdk.get_Data_js())
-
-#	JavaScript.eval("console.log(player)")
-	
-#func change_tutorial_var():
-##	tutorial_done = 
-#	pass
-
-var ft = true
-#var st = false
-
-func _process(delta):
-	pass
-#	if Ysdk.starting_player_logged and data_missing:
-#		print("STARTING PLAYER IS LOGGED IN")
-#		print(Ysdk.window.player)
-#		Ysdk.get_Data_js()
-##		our_data = Ysdk.returned_data.parse
-##		print(our_data)
-#		data_missing = false
-##		if our_data == null:
-##			print("data confirmed null")
-#
-##			first_launch = true
-#
-##			Ysdk.set_Data_js('{"tutorial_done": false, "current_level": 1}')
-###			yield(get_tree().create_timer(5.0), "timeout")
-##			Ysdk.get_Data_js()
-##			yield(get_tree().create_timer(5.0), "timeout")
-##			print(Ysdk.returned_data.result["tutorial_done"])
-##		else:
-##			if our_data["tutorial_done"]:
-##				current_level = our_data["current_level"]
-##				print(tutorial_done, current_level)
-#	elif data_missing:
-#		print("data is missing and the starting player hasn't logged in. opt for local")
-#		Ysdk.local_get_Data_js()
-##		our_data = Ysdk.returned_data.parse
-##		print(our_data)
-#		data_missing = false
-##		if our_data == null:
-##			print("data confirmed null in starting player not logged")
-#
-##	if st:
-##		st = false
-##		print(Ysdk.get_Data_js())
-##	if ft:
-##		ft = false
-##		st = true
-#	if Ysdk.ready_to_get_data and ft:
-#		ft = false
-#		print("ayo")
-#		if not Ysdk.starting_player_logged:
-#			Ysdk.local_get_Data_js()
-#			print("player didn't log in")
-#			going_local = true
-#			if typeof(Ysdk.returned_data) != TYPE_DICTIONARY:
-#				print("returned local data is good")
-#				level_setter_ysdk()
-##				print("tutorial done?")
-##				tutorial_done = bool(Ysdk.returned_data.result["tutorial_done"])
-##				print(tutorial_done)
-##				print("current level?")
-##				current_level = int(Ysdk.returned_data.result["current_level"])
-##				print(current_level)
-#			else:
-#				print("returned local data is {}")
-#				tutorial_done = false
-#				current_level = 1
-#		else:
-#			if not Ysdk.yandex_is_connected:
-#				Ysdk.local_get_Data_js()
-#				print("yandex SDK init failed for whatever reason")
-#				going_local = true
-#				if typeof(Ysdk.returned_data) != TYPE_DICTIONARY:
-#					print("returned local data is good")
-#					level_setter_ysdk()
-##					print("tutorial done?")
-##					tutorial_done = bool(Ysdk.returned_data.result["tutorial_done"])
-##					print(tutorial_done)
-##					print("current level?")
-##					current_level = int(Ysdk.returned_data.result["current_level"])
-##					print(current_level)
-#				else:
-#					print("returned local data is {}")
-#					tutorial_done = false
-#					current_level = 1
-#			else:
-#				level_setter_ysdk()
-##				print("tutorial done?")
-##				tutorial_done = bool(Ysdk.returned_data.result["tutorial_done"])
-##				print(tutorial_done)
-##				print("current level?")
-##				current_level = int(Ysdk.returned_data.result["current_level"])
-##				print(current_level)
 
 func level_setter_ysdk():
 	print("tutorial done?")
@@ -195,21 +71,41 @@ func level_setter_ysdk():
 	current_level = int(Ysdk.returned_data.result["current_level"])
 	print(current_level)
 
+func level_setter_local():
+	print("tutorial done?")
+	tutorial_done = bool(Ysdk.local_returned_data.result["tutorial_done"])
+	print(tutorial_done)
+	print("current level?")
+	current_level = int(Ysdk.local_returned_data.result["current_level"])
+	print(current_level)
+
+var ft = true
+var localft = true
+
+func _process(delta):
+	if localft:
+		localft = false
+		Ysdk.local_get_Data_js()
+		print("print local from process, ", Ysdk.local_returned_data.result)
+		level_setter_local()
+	if ft and Ysdk.yandex_is_connected and Ysdk.starting_player_logged:
+		Events.disconnect("start_button_pressed", self, "change_children")
+		Ysdk.get_Data_js()
+		if Ysdk.ready_to_get_data:
+			print("print yandex from process, ", Ysdk.returned_data.result)
+			level_setter_ysdk()
+			ft = false
+			Events.connect("start_button_pressed", self, "change_children")
+	pass
 
 func change_children():
-	if tutorial_done == null:
-		tutorial_done = false
-	if current_level == null:
-		current_level = 1
 	print("changing children...")
-	if going_local:
-		print("we going local ayy")
-		print(Ysdk.local_set_Data_js(Ysdk.generate_ready_dict_in_string(tutorial_done, current_level)))
-		print(Ysdk.local_get_Data_js())
-	else:
-		print(Ysdk.set_Data_js(Ysdk.generate_ready_dict_in_string(tutorial_done, current_level)))
-		print(Ysdk.get_Data_js())
-#	if Ysdk.yandex_is_connected and Ysdk.starting_player_logged:	
+	if tutorial_done == null and current_level == null:
+		tutorial_done = false
+		current_level = 1
+	Ysdk.local_set_Data_js(Ysdk.generate_ready_dict_in_string(tutorial_done, current_level))
+	if Ysdk.yandex_is_connected and Ysdk.starting_player_logged:
+		Ysdk.set_Data_js(Ysdk.generate_ready_dict_in_string(tutorial_done, current_level))
 	$button_pressed.play()
 	if current_level <= 31: 
 		var instanced_child = scene.instance()
@@ -254,4 +150,3 @@ func sound_change(button_pressed):
 	if button_pressed == false:
 		AudioServer.set_bus_mute(master_sound, false)
 		Events.sound_state = true
-
